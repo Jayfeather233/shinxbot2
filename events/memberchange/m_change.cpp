79210b834277449240f09ec75b40360492ea44a9
jayfeather <jayfeather233@gmail.com>
2023-04-01 10:52:10 +0800

    update

--- events/memberchange/m_change.cpp
+++ events/memberchange/m_change.cpp
@@ -7,18 +7,21 @@ void m_change::process(Json::Value J){
     std::string name = get_username(J["user_id"].asInt64(), J["group_id"].asInt64());
     std::ostringstream oss;
     oss << name << " (***" << std::setw(3) << std::setfill('0') << J["user_id"].asInt64()%1000 << ")";
-    name = oss.str();
+    std::string name1 = oss.str();
+    oss.clear();
+    oss << name << " (" << J["user_id"].asInt64() << ")";
+    std::string name2 = oss.str();
 
     if(J["notice_type"].asString() == "group_decrease"){
         if(J.isMember("operator_id") && J["operator_id"].asInt64() == J["user_id"].asInt64()){
-            cq_send(name + "离开了我们……", "group", 0, J["group_id"].asInt64());
+            cq_send(name1 + " 退群啦", "group", 0, J["group_id"].asInt64());
         } else {
-            cq_send(name + "被" + get_username(J["operator_id"].asInt64(), J["group_id"].asInt64()) + "送走了", "group", 0, J["group_id"].asInt64());
+            cq_send(name2 + " 被 " + get_username(J["operator_id"].asInt64(), J["group_id"].asInt64()) + " 送飞机票啦", "group", 0, J["group_id"].asInt64());
         }
         setlog(LOG::INFO, "member decrease in group " + std::to_string(J["group_id"].asInt64()) + " by "
             + std::to_string(J["user_id"].asInt64()) + " op " + std::to_string(J["operator_id"].asInt64()));
     } else if(J["notice_type"].asString() == "group_increase"){
-        cq_send((std::string)"欢迎" + name + "的加入", "group", 0, J["group_id"].asInt64());
+        cq_send((std::string)"欢迎" + name1 + "的加入", "group", 0, J["group_id"].asInt64());
         setlog(LOG::INFO, "member increase in group " + std::to_string(J["group_id"].asInt64()) + " by "
             + std::to_string(J["user_id"].asInt64()));
     }
