5120e8d79ba43285ff40fa88e4492e2ee544fad9
jayfeather <jayfeather233@gmail.com>
2024-01-12 15:23:36 +0800

    remove empty files

--- src/functions/bilidown/bilidown.h
+++
@@ -1,8 +0,0 @@
-#include "processable.h"
-
-class AnimeImg : public processable {
-public:
-    void process(std::string message, const msg_meta &conf);
-    bool check(std::string message, const msg_meta &conf);
-    std::string help();
-};
