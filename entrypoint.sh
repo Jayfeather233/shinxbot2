d3eb11f5670865c7ea8cba05a331bc81e80013c7
jayfeather <jayfeather233@gmail.com>
2024-07-24 14:28:05 -0400

    new dependency describ

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get install -y build-essential wget cmake 
 apt-get install -y libcurl4-openssl-dev openssl libssl-dev libjsoncpp-dev libzip-dev
 apt-get install -y libjpeg-dev libpng-dev libtiff-dev libgif-dev libwebp-dev webp libmagick++-dev
+apt-get install -y libfmt-dev
 
 cd /tmp
 wget https://www.imagemagick.org/download/ImageMagick.tar.gz
