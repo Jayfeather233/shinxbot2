1bff54164a5a5fcd08f2efd153857417b5fb04af
jayfeather233 <jayfeather233@gmail.com>
2025-06-30 02:08:12 +0800

    bug fix in m_change

--- src/functions/memberchangef/m_change_f.cpp
+++ src/functions/memberchangef/m_change_f.cpp
@@ -11,9 +11,9 @@ const std::string WELCOME_MESSAGE_FILE = "./config/welcome_messages.json";
 void m_change_f::save_welcome_messages() {
     Json::Value root;
     for (const auto &pair : group_welcome_messages) {
-        root[std::to_string(pair.first)] = std::string(pair.second.begin(), pair.second.end());
+        root[std::to_string(pair.first)] = wstring_to_string(pair.second);
     }
-    root["default"] = std::string(default_welcome_message.begin(), default_welcome_message.end());
+    root["default"] = wstring_to_string(default_welcome_message);
 
     writefile(WELCOME_MESSAGE_FILE, root.toStyledString());
 }
@@ -22,12 +22,12 @@ void m_change_f::load_welcome_messages(){
     for (const auto &group : root.getMemberNames()) {
         try {
             groupid_t group_id = std::stoull(group);
-            group_welcome_messages[group_id] = std::wstring(root[group].asString().begin(), root[group].asString().end());
+            group_welcome_messages[group_id] = string_to_wstring(root[group].asString());
         } catch (const std::exception &e) {
             ;
         }
     }
-    default_welcome_message = std::wstring(root["default"].asString().begin(), root["default"].asString().end());
+    default_welcome_message = string_to_wstring(root["default"].asString());
 }
 std::wstring m_change_f::get_welcome_message(groupid_t group_id){
     if (group_welcome_messages.find(group_id) != group_welcome_messages.end()) {
@@ -40,20 +40,20 @@ std::wstring m_change_f::format_message(const std::wstring &message, const msg_m
     std::string username = get_username(conf.p, conf.user_id, conf.group_id);
     size_t pos = formatted_message.find(L"{{username}}");
     while (pos != std::wstring::npos) {
-        formatted_message.replace(pos, 12, std::wstring(username.begin(), username.end()));
+        formatted_message.replace(pos, 12, string_to_wstring(username));
         pos = formatted_message.find(L"{{username}}");
     }
     return formatted_message;
 }
 void m_change_f::process(std::string message, const msg_meta &conf)
 {
-    std::wstring message_w = std::wstring(message.begin(), message.end());
+    std::wstring message_w = string_to_wstring(message);
     if (conf.message_type == "internal") {
         std::wstring welcome_message = trim(this->format_message(this->get_welcome_message(conf.group_id), conf));
         if (!welcome_message.empty()) {
-            conf.p->cq_send(std::string(welcome_message.begin(), welcome_message.end()),
+            conf.p->cq_send(wstring_to_string(welcome_message),
                 (msg_meta){std::string("group"), conf.user_id, conf.group_id, conf.message_id, conf.p});
-            conf.p->setlog(LOG::INFO, fmt::format("{} 入群消息: {}", conf.group_id, std::string(welcome_message.begin(), welcome_message.end())));
+            conf.p->setlog(LOG::INFO, fmt::format("{} 入群消息: {}", conf.group_id, wstring_to_string(welcome_message)));
         }
     } else {
         if (message_w.find(L"设置入群消息") == 0) {
@@ -64,9 +64,9 @@ void m_change_f::process(std::string message, const msg_meta &conf)
             std::wstring new_msg = message_w.substr(6);
             this->group_welcome_messages[conf.group_id] = new_msg;
             this->save_welcome_messages();
-            conf.p->setlog(LOG::INFO, fmt::format("{} 设置入群消息: {}", conf.group_id, std::string(new_msg.begin(), new_msg.end())));
+            conf.p->setlog(LOG::INFO, fmt::format("{} 设置入群消息: {}", conf.group_id, wstring_to_string(new_msg)));
             std::wstring test_msg = trim(this->format_message(this->get_welcome_message(conf.group_id), conf));
-            conf.p->cq_send(fmt::format("设置入群消息成功!\n{}", std::string(test_msg.begin(), test_msg.end())), conf);
+            conf.p->cq_send(fmt::format("设置入群消息成功!\n{}", wstring_to_string(test_msg)), conf);
         } else if (message_w.find(L"删除入群消息") == 0) {
             group_welcome_messages[conf.group_id] = L"";
             conf.p->setlog(LOG::INFO, fmt::format("{} 删除入群消息", conf.group_id));
@@ -79,11 +79,6 @@ void m_change_f::process(std::string message, const msg_meta &conf)
 }
 bool m_change_f::check(std::string message, const msg_meta &conf)
 {
-    conf.p->setlog(LOG::INFO, fmt::format("m_change_f check: {} {} {} {}", message,
-        conf.group_id,
-        conf.message_type,
-        conf.user_id
-    ));
     return conf.group_id != 0 && (
         message.find("设置入群消息") == 0
         || (conf.message_type == "internal" && message == "m_change_f")
